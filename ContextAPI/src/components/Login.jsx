d68d8224c23744b57f53ba99d3b1378c15a0775e
Zuned khan <zunedkhan107@gmail.com>
2025-02-18 18:52:18 +0530

    for brack

--- ContextAPI/src/components/Login.jsx
+++ ContextAPI/src/components/Login.jsx
@@ -14,18 +14,18 @@ const Login = () => {
         setUser({username, password})
     }
   return (
-    <div>
-        <h2>Login</h2>
-        <input type='text'
-        value={username}
+    <div className='flex justify-center mt-44 bg-emerald-900 p-8 flex-col rounded '>
+        <h2 className='flex justify-center items-center text-2xl m-2'>Login</h2>
+        <input required:true className='bg-emerald-50 outline-none border-2 m-2 rounded w-full placeholder-gray-7 ' type='text'
+        value={username} 
         onChange={(e) => setUsername(e.target.value) }
         placeholder='username'  />
         {" "}
-        <input type='text' 
+        <input required:true className='bg-emerald-50 m-2 outline-none border-2 rounded w-full placeholder-gray-7' type='text' 
         value={password}
         onChange={(e) => setPassword(e.target.value) }
         placeholder='password'  />
-        <button onClick={handleSubmit}>Submit</button>
+        <button className='bg-emerald-600 p-2 m-2 rounded w-full placeholder-gray-7' onClick={handleSubmit}>Submit</button>
     </div>
   )
 }
